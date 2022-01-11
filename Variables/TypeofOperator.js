ee253d3208463b27cfdb4a9f98362521ae4919be
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2022-01-11 11:23:10 -0600

    Adding previous Codecademy work

---
+++ Variables/TypeofOperator.js
@@ -0,0 +1,8 @@
+//Use console.log() to print the typeof newVariable.
+let newVariable = 'Playing around with typeof.';
+//Great, now let’s check what happens if we reassign the variable. Below the console.log() statement, reassign newVariable to 1.
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
+//Since you assigned this new value to newVariable, it has a new type! On the line below your reassignment, use console.log() to print typeof newVariable again.
+console.log(typeof newVariable);
