b28fc96e1f1f4e087f06e54c74ba92a006da2dd4
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2022-01-11 10:22:19 -0600

    Pushing previous work

---
+++ Variables/CreateAVariable-Let.js
@@ -0,0 +1,9 @@
+//Create a let variable called changeMe and set it equal to the boolean true.
+let changeMe = true;
+
+//On the line after changeMe is declared, set the value of changeMe to be the boolean false.
+changeMe = false;
+//To check if changeMe was reassigned, log the value saved to changeMe to the console.
+console.log(changeMe);
+
+
