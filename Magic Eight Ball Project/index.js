a685fd8b3e017a878947180b9161c84f2e00c579
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2022-01-17 14:26:32 -0600

    Adding magic eightBall work

---
+++ Magic Eight Ball Project/index.js
@@ -0,0 +1,43 @@
+let userName = 'Clay';
+userName ? console.log(`Hello, ${userName}!`) : 
+console.log('Hello!');
+let userQuestion = 'How is the weather outside?';
+console.log(`The user asked : ${userQuestion}`);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+  eightBall = 'It is certain'
+  break;
+case 1:
+  eightBall = 'It is decidely so'
+  break;
+case 2:
+  eightBall = 'Reply hazy try again'
+  break;
+case 3:
+  eightBall = 'Cannot predict now'
+  break;
+case 4:
+  eightBall = 'Do not count on it'
+  break;
+case 5:
+  eightBall = 'My sources say no'
+  break;
+case 6:
+  eightBall = 'Outlook not so good'
+  break;
+case 7:
+  eightBall = 'Signs point to yes'
+  break; 
+};
+
+console.log(`The eight ball answered: ${eightBall}`);
+
+
+// Hello, Clay!
+// The user asked : How is the weather outside?
+// The eight ball answered: It is decidely so
+
+//the eight ball will answer randomly each switch case
